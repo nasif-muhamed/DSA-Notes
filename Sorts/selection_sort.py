875e68203fa0a27e859ed6a0dd03cdd9457e7fb7
nasif-muhamed <muhdnasifk@gmail.com>
2025-11-06 11:15:02 +0530

    fix issue in selection sort

--- Sorts/selection_sort.py
+++ Sorts/selection_sort.py
@@ -2,7 +2,7 @@ def selection_sort(arr):
     for i in range(len(arr) - 1):
         smallest = i
         for j in range(i+1, len(arr)):
-            if arr[j] < arr[i]:
+            if arr[j] < arr[smallest]:
                 smallest = j
         if smallest != i:
             arr[i], arr[smallest] = arr[smallest], arr[i]
